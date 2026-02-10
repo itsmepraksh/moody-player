3468a37cb7ea02c30bbd4e8f30bc0bd4c0a7f6f8
prakash samanta <itsmepraksh.work@gmail.com>
2026-02-10 09:47:34 +0530

    fixed deployment issue for routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,6 @@
 
 const express = require('express')
-const cors = require('cors')
+// const cors = require('cors')
 const path =require('path')
 const app = express()
 const songRoute = require('./routes/song.route')
@@ -9,18 +9,19 @@ const userRoute = require('./routes/user.route')
 const cookieParser = require('cookie-parser')
 const authMiddleware = require('./middleware/auth.middleware')
 
-app.use(cors({
-    origin: process.env.FRONTEND_ORIGIN,
-    methods : ["GET","POST","PATCH","DELETE"],
-    credentials : true
-}))
+// app.use(cors({
+//     origin: process.env.FRONTEND_ORIGIN,
+//     methods : ["GET","POST","PATCH","DELETE"],
+//     credentials : true
+// }))
 
 // console.log(process.env.FRONTEND_ORIGIN)
 
 app.use(express.json())
 app.use(express.urlencoded())
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname,'../public')));
+// app.use(express.static(path.join(__dirname,'../public')));
+app.use(express.static("public"))
 
 app.get("*name",(req,res)=>{
     res.sendFile(path.join(__dirname,'../public/index.html'))
